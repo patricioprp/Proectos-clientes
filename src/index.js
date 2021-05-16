f16a72174603e7c32db037f12292370233ff7e58
Patricio <patricioprp06@gmail.com>
2021-05-16 15:41:52 -0300

    mock para que el cliente funcione sin un server

--- src/index.js
+++ src/index.js
@@ -16,3 +16,4 @@ ReactDOM.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 //este es un mock para poder desarrollar las funcionalidades del cliente sin necesidad de tener un back
+//en esta rama no se desarrollo la logica para conectare realmente con el back
